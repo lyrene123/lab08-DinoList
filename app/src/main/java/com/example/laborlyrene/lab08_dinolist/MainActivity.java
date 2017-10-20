e5857292cfc67ab492a8a46194796aad44b70477
Lyrene Labor <laborlyrene@LyreneMac.local>
2017-10-20 15:09:38 -0400

    First commit

---
+++ app/src/main/java/com/example/laborlyrene/lab08_dinolist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.laborlyrene.lab08_dinolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
